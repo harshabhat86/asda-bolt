0816fe163bdd7861f46b15f12c222516fb2d6161
a0c074e <alvin.chan@walmartlabs.com>
2020-08-21 01:31:21 -0700

    SVG fix

--- src/components/Trolley.jsx
+++ src/components/Trolley.jsx
@@ -8,10 +8,6 @@ import 'assets/scss/Trolley.scss';
 const Trolley = () => (
   <div className="trolley__container">
     <img src={trolley} />
-    <div className="trolley__text">
-      <span className="trolley__top-text">Ohh... it's empty</span>
-      <span className="trolley__bottom-text">Get started here</span>
-    </div>
     <div className="trolley__buttons">
       <img src={one} />
       <img src={two} />
